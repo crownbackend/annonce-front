47de10292c2c1a465298702fe32249f2f4ec0d86
Belhassen <crownbackend@gmail.com>
2022-07-07 19:49:44 +0200

    add bootstrap react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './component/header';
 
 function App() {
   return (
     <div className="App">
-      test
+      <Header/>
     </div>
   );
 }
