8446e65276ee0597916cf7c7d0d9346ad978df5c
Belhassen <crownbackend@gmail.com>
2022-07-13 19:41:02 +0200

    react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './component/header';
+import Header from './component/Header';
+import { Routes, Route, Link } from "react-router-dom";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+        <Routes>
+            <Route path="/inscription" element={<Register />} />
+        </Routes>
     </div>
   );
 }
---
+++ src/pages/Register.js
@@ -0,0 +1,13 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+function Register() {
+
+    return(
+        <div>
+<h1>je suis ici</h1>
+        </div>
+    )
+}
+
+export default Register;
