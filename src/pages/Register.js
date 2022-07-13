4053e0bc5245fc305aca348829b213c6ee735f01
Belhassen <crownbackend@gmail.com>
2022-07-13 19:56:39 +0200

    register form

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,4 +1,4 @@
-import { Container, Form, Button } from "react-bootstrap";
+import { Container, Form, Button, Col } from "react-bootstrap";
 
 function Register() {
 
@@ -8,17 +8,37 @@ function Register() {
             <h1>Formulaire d'inscription</h1>
             <br/>
             <Form>
+                <div className="row">
+                    <Form.Group as={Col} md="6" controlId="validationCustom01">
+                        <Form.Label>Nom</Form.Label>
+                        <Form.Control
+                            required
+                            type="text"
+                        />
+                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group as={Col} md="6" controlId="validationCustom02">
+                        <Form.Label>Prénom</Form.Label>
+                        <Form.Control
+                            required
+                            type="text"
+                        />
+                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                    </Form.Group>
+                </div>
+
+
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Label>Mail</Form.Label>
+                    <Form.Control type="email" placeholder="Mail" />
                     <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
+                        Entrer une adresse mail valide, vous aller recevoir un mail pour confirmer votre compte.
                     </Form.Text>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Label>Mot de passe</Form.Label>
+                    <Form.Control type="password" placeholder="Mot de passe" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
