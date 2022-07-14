f41a2f9ecdd9b1e10d18ff2b39dd23e75dc77a42
Belhassen <crownbackend@gmail.com>
2022-07-14 15:39:22 +0200

    format date

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,13 +1,31 @@
 import { Container, Form, Button, Col } from "react-bootstrap";
-import React from 'react';
+import React, { useState } from 'react';
 import UserApi from "../services/UserApi";
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+import fr from 'date-fns/locale/fr';
 
 function Register() {
-    const [firstName, setFirstName] = React.useState('');
-    const [lastName, setLastName] = React.useState('');
-    const [email, setEmail] = React.useState('');
-    const [password, setPassword] = React.useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [selected, setSelected] = React.useState(new Date())
 
+
+    const formatDate = (date) => {
+        let d = new Date(date);
+        let month = (d.getMonth() + 1).toString();
+        let day = d.getDate().toString();
+        let year = d.getFullYear();
+        if (month.length < 2) {
+            month = '0' + month;
+        }
+        if (day.length < 2) {
+            day = '0' + day;
+        }
+        return [year, month, day].join('-');
+    }
 
     function handleSubmit(event) {
         event.preventDefault();
@@ -16,6 +34,7 @@ function Register() {
         data.append('lastName', lastName);
         data.append('email', email);
         data.append('plainPassword', password);
+        data.append('birthday', formatDate(selected));
         UserApi.getAPIServiceInstance().register(data)
             .then(response => {
                 console.log(response);
@@ -64,9 +83,16 @@ function Register() {
                     <Form.Label>Mot de passe</Form.Label>
                     <Form.Control type="password" placeholder="Mot de passe" value={password} onChange={(e) => setPassword(e.target.value)} />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
+                <hr/>
+                <label>Date de naissance</label>
+                <DayPicker
+                    mode="single"
+                    selected={selected}
+                    onSelect={setSelected}
+                    fromYear={1900} toYear={2025} captionLayout="dropdown"
+                    locale={fr}
+                />
+                <hr/>
                 <Button variant="primary" type="submit">
                     Envoyer
                 </Button>
